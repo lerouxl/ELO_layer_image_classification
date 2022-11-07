a32b9d472e73be615927eb6570fa82dd3d6fb5ff
Leopold <lerouxl@cardiff.ac.uk>
2022-11-07 22:12:49 +0000

    Add model training script

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ This script had been tested on python 3.7, and no GPU support were added.
 2. Virtual environment:
    1. Install python 3.7 if needed.
    2. Create your environment: `virtualenv venv`.
-   3. Activate it: `source venv/bin/activate`.
+   3. Activate it: `source venv/bin/activate` or `.\venv\Scripts\activate` on windows.
    4. Install dependencies: `pip install -r requirements.txt`.
 
 
@@ -61,7 +61,58 @@ By clinking it's possible to zoom on the image.
 Hovering the mouse on a square allow to see its classification name and the score of each class.
 ![alt text](https://github.com/lerouxl/ELO_layer_image_classification/blob/main/data/Area_classification_results_zoom.png?raw=true)
 
+____
+## Train on new dataset
 
+To generate a new AI model, the `train.py` script can be used.
+This script require to have a dataset split in 2 folders (`train` and `val`) made of subfolder named with the class names of their pictures as describe bellow:
+```
+dataset_name
+|_>train
+|   |___>class_1
+|   |___>class_2
+|   |___>class_3
+|   |___>class_4
+|   |___>class_4 
+|_>val
+   |___>class_1
+   |___>class_2
+   |___>class_3
+   |___>class_4
+   |___>class_4 
+```
 
+Then the `train_dataloader` and `val_dataloader` must be updated to train the AI on the new dataset. For this, the image folder must be modified to be:
 
+```python
+def train_dataloader(self):
+        train_dataset = torchvision.datasets.ImageFolder("./data/dataset_name/train", transform=self.transform)
+        
+        return torch.utils.data.DataLoader(train_dataset, batch_size=self.hparams.batch_size)
 
+def val_dataloader(self):
+   val_dataset = torchvision.datasets.ImageFolder("./data/dataset_name/val", transform=self.transform)
+   return torch.utils.data.DataLoader(val_dataset, batch_size=self.hparams.batch_size)
+```
+With `dataset_name` the name of the new dataset folder (saved in the data folder).
+
+*Note*: During training, check that your model is training on the GPU and note the CPU, otherwise, this will take lot of time to train.
+
+At the end of the training, a `weights.pt` file will be saved containing the trained network. It can be loaded with `ImageClassifier.load_from_checkpoint("weights.pt")`. To use it, modify the `get_model` function from `src/model.py` by adding the path of the new model:
+
+```python
+def get_model(path: str) -> ImageClassifier:
+    """
+    Load a saved pytorch lightning model from a path.
+    The model is put in eval mode and gradient is deactivated.
+    :param path: path where is saved the model
+    :return: ImageClassifier model with loaded weitgh.
+    """
+    model = ImageClassifier.load_from_checkpoint(r"model/weights.pt")
+
+    for p in model.parameters():
+        p.requires_grad = False
+    model.eval()
+
+    return model
+```
---
+++ train.py
@@ -0,0 +1,107 @@
+import os
+import torch
+from torch import nn
+import torchvision.transforms as T
+from pytorch_lightning import LightningDataModule, LightningModule
+from pytorch_lightning.utilities.cli import LightningCLI
+from torch.nn import functional as F
+from torchmetrics import Accuracy
+import torch
+import torchvision
+
+def set_parameter_requires_grad(model):
+    for param in model.parameters():
+        param.requires_grad = False
+
+
+def get_squeezenet():
+    #model = torch.hub.load('pytorch/vision:v0.10.0', 'squeezenet1_0', pretrained=True)
+    model =  torchvision.models.squeezenet1_1(pretrained=True)
+    set_parameter_requires_grad(model)
+    
+    model.classifier[1] =  nn.Sequential(
+        nn.Dropout(),
+        nn.Conv2d(512, 4, kernel_size=(1,1), stride=(1,1)),
+        nn.ReLU(),
+        nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        ) 
+    model.num_classes = 4
+    return model
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = get_squeezenet()
+
+    def forward(self, x):
+        x = self.net(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+class ImageClassifier(LightningModule):
+    def __init__(self, model=None, lr=1.0, gamma=0.7, batch_size=32):
+        super().__init__()
+        self.save_hyperparameters(ignore="model")
+        self.model = model or Net()
+        self.val_acc = Accuracy()
+
+        checkpoint_path = os.path.join(os.path.dirname(__file__), "weights.pt")
+        if os.path.exists(checkpoint_path):
+            self.load_state_dict(torch.load(checkpoint_path).state_dict())
+
+    @property
+    def example_input_array(self):
+        return torch.zeros((1, 3, 196, 196))
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.forward(x)
+        loss = F.nll_loss(logits, y.long())
+        self.log("train_loss", loss, on_step=True, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.forward(x)
+        loss = F.nll_loss(logits, y.long())
+        self.log("val_acc", self.val_acc(logits, y), on_step=True, on_epoch=True)
+        self.log("val_loss", loss)
+
+    def configure_optimizers(self):
+        return torch.optim.Adadelta(self.model.parameters(), lr=self.hparams.lr)
+
+
+class DataModule(LightningDataModule):
+    def __init__(self, batch_size=32):
+        super().__init__()
+        self.save_hyperparameters()
+
+    @property
+    def transform(self):
+        preprocess = T.Compose([T.Resize((196, 196)),
+                           T.ToTensor(),
+                           T.ConvertImageDtype(torch.float),
+                           T.Normalize(mean=(0.485, 0.456, 0.406),
+                                       std=(0.229, 0.224, 0.225))])
+        return preprocess
+
+
+
+    def train_dataloader(self):
+        train_dataset = torchvision.datasets.ImageFolder("./data/ELO_jpg_subimages_4_class/train", transform=self.transform)
+        
+        return torch.utils.data.DataLoader(train_dataset, batch_size=self.hparams.batch_size)
+
+    def val_dataloader(self):
+        val_dataset = torchvision.datasets.ImageFolder("./data/ELO_jpg_subimages_4_class/val", transform=self.transform)
+        return torch.utils.data.DataLoader(val_dataset, batch_size=self.hparams.batch_size)
+
+
+if __name__ == "__main__":
+    cli = LightningCLI(
+        ImageClassifier, DataModule, seed_everything_default=42, save_config_overwrite=True, run=False
+    )
+    cli.trainer.fit(cli.model, datamodule=cli.datamodule)
